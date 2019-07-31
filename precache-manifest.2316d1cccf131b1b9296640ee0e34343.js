c359d419012d7047c7e35dfad6985f70d765d584
minh <hoangminh160997@gmail.com>
2019-07-31 01:25:21 -0700

    Updates

---
+++ precache-manifest.2316d1cccf131b1b9296640ee0e34343.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d416c47939914a72ad8c8adc8631f4d",
+    "url": "/marvell-hero/index.html"
+  },
+  {
+    "revision": "af853d743c009a760c26",
+    "url": "/marvell-hero/static/js/2.30b8f2c4.chunk.js"
+  },
+  {
+    "revision": "1c2d888482a4c10e6aa0",
+    "url": "/marvell-hero/static/js/main.6bd451c1.chunk.js"
+  },
+  {
+    "revision": "11a530dbeb1de2f6f0c1",
+    "url": "/marvell-hero/static/js/runtime~main.1b469d40.js"
+  }
+]);
